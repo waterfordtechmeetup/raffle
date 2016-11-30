5d35080814e313a83abe651a7f8ef08f35b53e8e
Jason Madigan <mad@redhat.com>
2016-11-30 15:18:02 +0000

    Profile link

--- public/javascript/draw.js
+++ public/javascript/draw.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     $.post("/draw", function(winner) {
       console.log(winner);
       $(".winner_name").text(winner.member.name);
+      $(".profile_url").attr('href', "https://www.meetup.com/Waterford-Tech-Meetup/members/" + winner.member.member_id);
+
       if (winner.member_photo && winner.member_photo.photo_link) {
         $(".polaroid").show();
         $(".winner_photo").attr('src', winner.member_photo.photo_link);
